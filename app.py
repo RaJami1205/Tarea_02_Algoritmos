63446d109339a3307501215cfcfb2bd471c7b6c8
RaJami1205 <rauljamiralfarorodriguez512@gmail.com>
2024-10-14 20:46:01 -0600

    Intento de conexion entre interfaz y consola

--- app.py
+++ app.py
@@ -6,10 +6,22 @@ from flask import Flask, render_template, request, redirect, flash , send_file, 
 app = Flask(__name__)
 app.secret_key = 'ra&diegop'
 
-# Ruta para el menú principal
+# Ruta principal
 @app.route('/')
 def index():
     return render_template('interfaz.html')
+
+# Ruta para procesar el formulario
+@app.route('/ejecutar', methods=['POST'])
+def ejecutar():
+    valores = request.form['lista']
+    limite = request.form['limite']
+
+    for x in valores:
+        print(x)
+    return
+
+    return render_template('resultado.html', resultado=resultado)
 
 # Iniciamos la plantilla web
 if __name__ == '__main__':
